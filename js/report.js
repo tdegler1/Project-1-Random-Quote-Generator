30a9e6c93cc913175caffb79e0f8a4f9325defbd
tdegs <tdegs999@gmail.com>
2019-06-14 17:57:08 -0400

    Initial commit

---
+++ js/report.js
@@ -0,0 +1,38 @@
+var message = '';
+var randomQuote;
+var search;
+
+function print(message) {
+  var outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+  for (var i = 0; i < quotes.length; i += 1) {
+    randomQuote = quotes[i];
+	console.log (randomQuote.quote);
+	console.log (randomQuote.source);
+	console.log (randomQuote.citation);
+	console.log (randomQuote.year);
+  }
+
+
+DOGS:
+Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read. Groucho Marx
+Read more at: https://www.brainyquote.com/quotes/groucho_marx_137218?src=t_pet
+
+Scratch a dog and you'll find a permanent job. Franklin P. Jones
+
+What counts is not necessarily the size of the dog in the fight - it's the size of the fight in the dog. Dwight D. Eisenhower
+
+MOVIES:
+“Toto, I've got a feeling we're not in Kansas anymore.” The Wizard of Oz, 1939
+
+“Of all the gin joints in all the towns in all the world, she walks into mine.” Casablanca, 1942
+
+“My precious.” The Lord of the Rings: Two Towers, 2002
+
+PHILOSOPHY:
+I have no special talent. I am only passionately curious. Albert Einstein 
+If you judge people, you have no time to love them. Mother Teresa
+The weak can never forgive. Forgiveness is the attribute of the strong. Mahatma Gandhi
+Genius is one percent inspiration, ninety-nine percent perspiration. Thomas A. Edison
---
+++ js/script.js
@@ -0,0 +1,32 @@
+var quotes = [ 
+  { 
+    quote: "Here's Quote #1",
+    source: "Author #1",
+    citation: "The movie #1 it came from",
+    year: 1901
+  },
+  { 
+    quote: "Here's Quote #2",
+    source: "Author #2",
+    citation: "The movie #2 it came from",
+    year: 1902
+  },
+	{ 
+    quote: "Here's Quote #3",
+    source: "Author #3",
+    citation: "The movie #3 it came from",
+    year: 1903
+  },
+	{ 
+    quote: "Here's Quote #4",
+    source: "Author #4",
+    citation: "The movie #4 it came from",
+    year: 1904
+  },
+  {
+    quote: "Here's Quote #5",
+    source: "Author #5",
+    citation: "The movie #5 it came from",
+    year: 1905
+  }
+];
