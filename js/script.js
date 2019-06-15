7ab0ed8655e4e4d72db0ff2d73cd80665589b6d6
tdegs <tdegs999@gmail.com>
2019-06-15 15:14:54 -0400

    added real quotes and exceed code

--- js/script.js
+++ js/script.js
@@ -3,39 +3,81 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
+/*** BEGIN Global Variables ***/
+
 /* Create array of quote objects. 
-All quotes have a quote and source(author) property; optional properties include citation and year of the quote. */
+All quotes have a quote and source(author) property; optional properties include citation, year and category of the quote. */
 var quotes = [ 
-  { 
-    quote: "Here's Quote #1",
-    source: "Author #1",
-    citation: "The movie #1 it came from",
-    year: 1901
-  },
-  { 
-    quote: "Here's Quote #2",
-    source: "Author #2",
-	citation: "The movie #2 it came from",
-    year: 1902
+   { 
+    quote: "The essence of genius is to know what to overlook.",
+	source: "William James",
+	category: "Genius"
   },
 	{ 
-    quote: "Here's Quote #3",
-    source: "Author #3"
+    quote: "There's a fine line between genius and insanity. I have erased this line.",
+	source: "Oscar Levant",
+	category: "Genius"
   },
 	{ 
-    quote: "Here's Quote #4",
-    source: "Author #4",
-    year: 1904
+    quote: "Intellectuals solve problems; geniuses prevent them.",
+	source: "Albert Einstein",
+	category: "Genius"
+  },
+	{
+    quote: "Be the person your dog thinks you are.",
+    source: "J.W. Stephens",
+	category: "Dogs"
+  },
+  { 
+    quote: "Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read.",
+    source: "Groucho Marx",
+	category: "Dogs"
+  },
+	{ 
+    quote: "Scratch a dog and you'll find a permanent job.",
+    source: "Franklin P. Jones",
+	category: "Dogs"
+  },
+    { 
+    quote: "KHAAANNN!",
+    source: "Captain James T. Kirk",
+    citation: "Star Trek II: The Wrath of Khan",
+    year: 1982,
+	category: "Movies"
+  },
+	{ 
+    quote: "Toto, I've got a feeling we're not in Kansas anymore.",
+	source: "Dorothy Gale",
+	citation: "The Wizard of Oz",
+    year: 1939,
+	category: "Movies"
   },
   {
-    quote: "Here's Quote #5",
-    source: "Author #5",
-    citation: "The movie #5 it came from"
+    quote: "My precious.",
+    source: "Gollum",
+    citation: "The Lord of the Rings: Two Towers",
+	year: 2002,
+	category: "Movies"
   }
 ];
 
-/* Generate a random number between 0 and the last index in the 'quotes' array and retrieve a random quote object from the `quotes` array. 
-Return the random quote. */
+/*** END Global Variables ***/
+
+/*** BEGIN Functions ***/
+
+/* Generate three random numbers for red, blue and green. 
+Create HTML string of RGB color values and store in variable 'color'.
+Add background color attribute to the body tag (with id='page') */
+function randomRGB() {
+	var color = 'rgb('; // initialize color string
+  	color += Math.floor(Math.random() * 256 ) + ','; // red
+  	color += Math.floor(Math.random() * 256 ) + ','; // blue
+  	color += Math.floor(Math.random() * 256 ) + ')'; // green
+	var targetDiv = document.getElementById('page');
+	targetDiv.style.backgroundColor = color;
+}
+
+/* Generate a random number between 0 and the last index in the 'quotes' array and retrieve a random quote object from the `quotes` array. */
 function getRandomQuote () {
 	var randomNumber = Math.floor(Math.random() * quotes.length );
 	return quotes[randomNumber];
@@ -56,13 +98,35 @@ function printQuote() {
 	if (randomQuote.year) {
 		html += '<span class="year">' + randomQuote.year + '</span>';
 	}
+	if (randomQuote.category) {
+		html += '<span class="category"> (' + randomQuote.category + ')</span>';
+	}
 	html += '</p>';
+	/* Call 'randomRGB' function to change the background color of the webpage */
+	randomRGB();
+	/* Display the new random quote on the webpage */
 	var outputDiv = document.getElementById('quote-box');
 	outputDiv.innerHTML = html;
 }
 
+/*** END Functions ***/
+
+/*** BEGIN Triggers ***/
+
 /* When "Show another quote" button is clicked, call the printQuote function. */
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+/*** END Triggers ***/
+
+/*** BEGIN Main Program ***/
+
 /* Initialize the web page with the first random quote */
 printQuote();
+
+/* Auto-refresh the quote (and background color) every 20 seconds. */
+setInterval(function(){ 
+	printQuote(); }, 
+	20000);
+
+/*** END Main Program ***/
+
